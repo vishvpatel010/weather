a8714c6e6c73ed7061adfbd1f64e3dc9d1760ca1
Vishv Patel <vishv@Vishvs-MacBook-Air.local>
2023-07-31 20:33:17 +0530

    Curent Location

--- public/js/main.js
+++ public/js/main.js
@@ -9,15 +9,41 @@ const datahide = document.querySelector(".middle_layer");
 
 const now = new Date();
 
+// Set Current Day
 let date = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 day.innerText= date[now.getDay()];
 
+// Set Current Date
 let mon = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEPT', 'OCT', 'NOV', 'DEC'];
 today_date.innerText=`${now.getDate()} ${mon[now.getMonth()]}`;
 
+// Get Location of Current location
+const myFunction = async()=>{
+    try {
+        const position = await new Promise((resolve, reject) => {
+            if (window.navigator && window.navigator.geolocation) 
+            {
+                window.navigator.geolocation.getCurrentPosition(resolve, reject);
+            } 
+            else 
+            {
+                reject(new Error('Geolocation is not available.'));
+            }
+        });
+        let lat = position.coords.latitude;
+        let log = position.coords.longitude;
+        // console.log(log);
+        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&units=metric&appid=32344f629879865ee264be3f83d64be7`;
+            let response = await fetch(url);
+            const data=await response.json();
+            const arrData = [data];
+        setData(arrData);
+      } catch (error) {
+        console.error('Error retrieving weather data:', error);
+      }
+}
 
-
-
+// Get Location Based on user enterd location
 const getInfo = async(event)=>{
     event.preventDefault(); 
     let cityVal = cityName.value;
@@ -32,27 +58,7 @@ const getInfo = async(event)=>{
             const data=await response.json();
             const arrData = [data];
 
-            city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
-            temp.innerText = arrData[0].main.temp;
-
-            let tempMod = arrData[0].weather[0].main;
-            
-            if(tempMod == 'Clear')
-                temp_status.innerHTML = `<img src="img/clear.png" alt="clear">`;
-            else if(tempMod == 'Clouds')
-                temp_status.innerHTML = `<img src="img/clouds.png" alt="clouds">`;
-             else if(tempMod == "Drizzle")
-                temp_status.innerHTML = `<img src="img/drizzle.png" alt="drizzle">`;
-            else if(tempMod == 'Rain')
-                temp_status.innerHTML = `<img src="img/rain.png" alt="rain">`;
-            else if(tempMod == 'Snow')
-                    temp_status.innerHTML = `<img src="img/snow.png" alt="snow">`;
-            else if(tempMod == "Thunderstorm")
-                temp_status.innerHTML = `<img src="img/thunderstorm.png" alt="thunderstorm">`;
-            else
-                temp_status.innerHTML = `<img src="img/clear.png" alt="clear">`;
-
-            console.log(arrData);
+            setData(arrData);
 
             datahide.classList.remove("data_hide");
         }catch(e){
@@ -62,6 +68,30 @@ const getInfo = async(event)=>{
         
     }
     
+};
+
+
+// Set Weather data as per location
+const setData = (arrData)=>{
+    city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
+    temp.innerText = arrData[0].main.temp;
+
+    let tempMod = arrData[0].weather[0].main;
+    
+    if(tempMod == 'Clear')
+        temp_status.innerHTML = `<img src="img/clear.png" alt="clear">`;
+    else if(tempMod == 'Clouds')
+        temp_status.innerHTML = `<img src="img/clouds.png" alt="clouds">`;
+        else if(tempMod == "Drizzle")
+        temp_status.innerHTML = `<img src="img/drizzle.png" alt="drizzle">`;
+    else if(tempMod == 'Rain')
+        temp_status.innerHTML = `<img src="img/rain.png" alt="rain">`;
+    else if(tempMod == 'Snow')
+            temp_status.innerHTML = `<img src="img/snow.png" alt="snow">`;
+    else if(tempMod == "Thunderstorm")
+        temp_status.innerHTML = `<img src="img/thunderstorm.png" alt="thunderstorm">`;
+    else
+        temp_status.innerHTML = `<img src="img/clear.png" alt="clear">`;
 };
 
 submitBtn.addEventListener('click', getInfo);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
 const app = express();
-const port = process.env.PORT || 9030;
+const port = process.env.PORT || 9040;
 
 let staticPath = path.join(__dirname, '../public');
 let template_path = path.join(__dirname, '../templates/views');
